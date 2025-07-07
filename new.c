b1c6548adddaa894bf108006cb1539872dd251d0
Mananas12 <mariam.hayrapetyan.4002@gmail.com>
2025-07-07 16:58:34 +0400

    third

--- new.c
+++ new.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
  int main () {
-     double a = 0;
-     printf("Enter the floating_point number: ");
-     scanf("%lf", &a);
-     printf("Entered value is: %lf\n ", a);
+     char symbol = 0;
+     printf("Enter the symbol: ");
+     scanf("%c", &symbol);
+     printf("ASCII value : %d\n ", symbol);
      return 0;
 }
 
