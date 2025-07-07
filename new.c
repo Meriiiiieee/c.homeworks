7a15290defe341ea20229385c976ccd716b2f048
Mananas12 <mariam.hayrapetyan.4002@gmail.com>
2025-07-07 16:49:01 +0400

    second

--- new.c
+++ new.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main () {
-int a = 0;
-printf("Enter the value: ");
-scanf("%d", &a);
-printf("Entered value is: %d\n ", a);
 
-return 0;
+ int main () {
+     double a = 0;
+     printf("Enter the floating_point number: ");
+     scanf("%lf", &a);
+     printf("Entered value is: %lf\n ", a);
+     return 0;
 }
-//
+
