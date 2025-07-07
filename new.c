c12903aa5281f6e9e38519805dc135a62be33fff
Mananas12 <mariam.hayrapetyan.4002@gmail.com>
2025-07-07 18:32:16 +0400

    twelve

--- new.c
+++ new.c
@@ -80,3 +80,14 @@
      printf("The result of area: %f", area);
      return 0;
 }
+//
+#include <stdio.h>
+ int main () {
+     float celsius = 0;
+     float fahrenheit = 0;
+     printf("Enter temperature in Celsius: ");
+     scanf("%f", &celsius);
+     fahrenheit = (celsius * 9/5) + 32;
+     printf("%.2f", celsius, fahrenheit);
+     return 0;
+}
