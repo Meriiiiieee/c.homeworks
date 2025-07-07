c6d9ff5569e6b71f22368536c6f2cff92929699c
Mananas12 <mariam.hayrapetyan.4002@gmail.com>
2025-07-07 17:30:15 +0400

    sixth

--- new.c
+++ new.c
@@ -9,4 +9,17 @@
      printf("Result of the expression (x / (5 + 2) + 30 * x - 51 ) : %f\n ",result);
      return 0;
 }
-
+//
+#include <stdio.h>
+ int main() {
+     int x = 0;
+     int y = 0;
+     float result = 0;
+     printf("Enter the value for x: ");
+     scanf("%d", &x);
+     printf("Enter the value for y: ");
+     scanf("%d", &y);
+     result = (x + y + 21 * x / y - 200);
+     printf("Result of the expression (x + y + 21 * x / y - 200) : %f\n",result);
+     return 0;
+}
