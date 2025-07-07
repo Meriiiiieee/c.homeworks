703d6cbd370576e0574231fe1e800484590d3c41
Mananas12 <mariam.hayrapetyan.4002@gmail.com>
2025-07-07 17:10:53 +0400

    fourth

--- new.c
+++ new.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
  int main () {
-     char symbol = 0;
-     printf("Enter the symbol: ");
-     scanf("%c", &symbol);
-     printf("ASCII value : %d\n ", symbol);
+     int x = 0;
+     float result = 0;
+     printf("Enter the value of x: ");
+     scanf("%d", &x);
+     result = (4 * x + 21 * x * x - 12);
+     printf("Result of the expression (4 * x + 21 * x * x - 12 ) : %f\n ",result);
      return 0;
 }
 
