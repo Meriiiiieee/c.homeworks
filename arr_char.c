4597e5cb41d2de7533f187ed460e7f83f6ad4a4d
Meriiiiieee <meri.hakobyan21@edu.ysu.am>
2025-07-28 17:13:57 +0400

    7

---
+++ arr_char.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SIZE 4
+
+int main () {
+    char arr[SIZE];
+
+    printf("Enter the elements: ");
+    for (int i = 0; i < SIZE; i++) {
+        scanf(" %c", &arr[i]);
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        if ( arr[i]>= '0' && arr[i]<='9') {
+            printf(" %c", arr[i]);
+
+        }
+    }
+
+     return 0;
+}
